970c38813c55603559288b151f2f77a850cca764
GuilhermeAugui <117306676+GuilhermeAugui@users.noreply.github.com>
2024-07-01 21:22:28 -0300

    interligando as classes

--- src/controler/saidadaDedinheiroControladora.js
+++ src/controler/saidadaDedinheiroControladora.js
@@ -1,37 +1,19 @@
 import daoAddDinheiro from src/DAO/daoAddDinheiro.js;
-import daoSaidaDeDinhiro from '/.src/DAo/daSaidaDeDinhiro.js';
+import daoSaidaDeDinhiro from /.src/DAo/daSaidaDeDinhiro.js;
 import { getSaldo, removeSaldo } from './addDinheiroCntroler';
 
-class RemoveDinheiro {
-    static removeDinheiro(valor) {
-        if (getSaldo() >= valor) {
-            removeSaldo(valor);
-            console.log(`Novo saldo: ${getSaldo()}`);
+// src/DAO/daSaidaDeDinheiro.js
+import SaldoManager from './SaldoManager.js';
+
+class SaidaDeDinheiro {
+    static removerDinheiro(valor) {
+        if (valor <= SaldoManager.getSaldo()) {
+            SaldoManager.atualizarSaldo(-valor);
+            // Aqui você pode adicionar lógica para interagir com daSaidaDeDinheiro, se necessário
         } else {
-            console.log('Saldo insuficiente para remover o valor solicitado.');
+            console.log("Saldo insuficiente para esta operação.");
         }
     }
 }
 
-export default RemoveDinheiro;
-// Certifique-se de que as classes compartilhem o mesmo saldo
-Object.defineProperty(RemoveDinheiro, 'saldo', {
-    get: function() {
-        return AddDinheiro.saldo;
-    },
-    set: function(value) {
-        AddDinheiro.saldo = value;
-    }
-});
-
-// Uso das classes
-
-// Configura o saldo inicial
-AddDinheiro.setSaldo(100);
-
-// Adiciona dinheiro ao saldo existente
-AddDinheiro.addDinheiro(50); // Novo saldo: 150
-
-// Remove dinheiro do saldo existente
-RemoveDinheiro.removeDinheiro(30); // Novo saldo: 120
-RemoveDinheiro.removeDinheiro(200); // Saldo insuficiente para remover o valor solicitado.
+export default SaidaDeDinheiro;
